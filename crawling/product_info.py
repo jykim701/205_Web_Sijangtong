993fa8c3dc0279835260355c0c622405c7dba7b9
jykim701 <58364280+jykim701@users.noreply.github.com>
2020-12-12 01:42:48 +0900

    login db and python code
    mongo db reconnected
    python code for crawling added

--- crawling/product_info.py
+++ crawling/product_info.py
@@ -1,14 +1,79 @@
+from flask import Flask, render_templte, jsonify, request
+import requests
 from bs4 import BeautifulSoup
-from urlib.request import urlopen
+from pymongo import MongoClient
 
-#상품 이름을 긁어오는 함수
-def product_name():
-    url = "https://shopping.naver.com/market/traditionalmarket?storeId=100840028"
-    
+client = MongoClient('localhost', 27017)
+crawlingDB = client.dbproject
+
+#성대전통시장 URL
+def get_product_info():
+    url = "https://shopping.naver.com/market/traditionalmarket?storeId=100840009"
+    header = {'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
     result = requests.get(url)
-    a = result.text
+    soup = BeautifulSoup(result.text, 'html.parser')
+    
+    #href 태그 속 market ID
+    marketID = ['100840011','100840017', '100840024','100938154','100840031','100840035','100840048','100840055','100840059','100840069','100840074',
+                '100859292','100939541','100840113','100840005','100840009','100840013','100839993','100840018','100942860','100840072','100839984',
+                '100840023','100840028','100840033','100840037','100840044','100840049','100840052','100840003','100840064']
 
-    soup = BeautifulSoup(a, 'html.parser')
+    #시장별 URL을 넣으면 된다
+    #파이썬 크롤링 코드로 이미 시장별 strodID를 모아와 list에 저장했기 때문에 stordID를 긁어오는 별도의 코드없이 바로 넣는다
+    for i in range(len(marketID)):
+        storeId = marketID[i]
+        result = requests.get(f"https://shopping.naver.com/market/traditionalmarket?storeId={storeId}")
 
-    link = soup.select('div.X1DfuFYD6M > a')
-    len(link)
+    #카테고리별 반복문 돌리기
+    data = BeautifulSoup(result.text, 'html.parser')
+    products = data.find('div', {'class':'jas_contain'})
+    product = products.find_all('div', {'class':'product-grid-item'})
+
+    for one in product:
+        title = one.select_one('.product-title').text
+        price = one.select_one('.price').text
+        img_h = one.select_one('.product-element-top > a').get('href')
+        img_url = f""
+        img_s = one.select_one('.product-element-top > a > img').get('src')
+        img_src = f"https:{img_s}"
+    
+    doc = {'market': keys[i], 'title':title, 'price':price, 'img_url':img_url, 'img_src':img_src}
+
+    def info():
+        url = "https://zerowastestore.com/"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    result = requests.get(url, headers=headers)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    products = soup.find('div', {'class': 'jas_contain'})
+    product = products.find_all(
+        'div', {'class': 'product-grid-item'})
+    for one in product:
+        main_title = one.select_one('.product-title').text
+        main_price = one.select_one('.price').text
+        main_img_h = one.select_one(
+            '.product-element-top > a').get('href')
+        main_img_url = f"https://zerowastestore.com{main_img_h}"
+        main_img_s = one.select_one(
+            '.product-element-top > a > img').get('src')
+        main_img_src = f"https:{main_img_s}"
+
+        main_doc = {
+            'category': 'arrival',
+            'main_title': main_title,
+            'main_price': main_price,
+            'main_img_url': main_img_url,
+            'main_img_src': main_img_src
+        }
+        db.zerowastestore.insert_one(main_doc)
+    
+
+def update_db():
+    update_soap = db.zerowastestore.update_many(
+        {'category': 'Soap Bars'}, {'$set': {'category': 'Others'}})
+    update_zwk = db.zerowastestore.update_many(
+        {'category': 'ZWS Kits'}, {'$set': {'category': 'Others'}})
+    update_zws = db.zerowastestore.update_many({'category': 'ZWS Sustainables'}, {'$set': {'category': 'Others'}})
+
+if __name__ == "__main__":
+    # get_info()
---
+++ node.js
@@ -0,0 +1,285 @@
+var express = require('express');
+var http = require('http');
+var serveStatic = require('serve-static');      //특정 폴더의 파일들을 특정 패스로 접근할 수 있도록 열어주는 역할
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var expressErrorHandler = require('express-error-handler');
+ 
+var mongoClient = require('mongodb').MongoClient;
+ 
+ 
+ 
+var database;
+ 
+//몽고디비에 연결 ,  보통 웹서버 만든 직후 연결 , DB 먼저 연결 되도 상관 없음
+//먼저 db를 가져온다 
+function connectDB() {
+    //localhost 로컬 호스트
+    //:27017  몽고디비 포트
+    //local db 생성시 만든 폴더 명
+    var databaseURL = 'mongodb+srv://sijangtong:205webprogramming@205.v8ndu.mongodb.net/SiJangTong.users?retryWrites=true&w=majority7';
+    mongoClient.connect(databaseURL,
+        function (err, cluster)
+        {
+            //이 구문까지 실행되었다면 ongoDB 에 연결된 것
+            if (err) {
+                console.log('db connect error');
+                return;
+            }
+ 
+            console.log('db was connected : ' + databaseURL);
+ 
+            database = cluster.db('test');
+ 
+            //var users = database.collection('users');
+ 
+ 
+        }
+    );
+ 
+}
+ 
+ 
+ 
+ 
+ 
+var app = express();      //express 서버 객체
+ 
+ 
+app.set('port', 3000);
+app.use(serveStatic(path.join('public', __dirname, 'public')));
+ 
+ 
+ 
+var bodyParser_post = require('body-parser');       //post 방식 파서
+//post 방식 일경우 begin
+//post 의 방식은 url 에 추가하는 방식이 아니고 body 라는 곳에 추가하여 전송하는 방식
+app.use(bodyParser_post.urlencoded({ extended: false }));            // post 방식 세팅
+app.use(bodyParser_post.json());                                     // json 사용 하는 경우의 세팅
+//post 방식 일경우 end
+ 
+ 
+ 
+app.use(serveStatic(path.join(__dirname, 'public')));
+ 
+ 
+//쿠키와 세션을 미들웨어로 등록한다
+app.use(cookieParser());
+ 
+//세션 환경 세팅
+//세션은 서버쪽에 저장하는 것을 말하는데, 파일로 저장 할 수도 있고 레디스라고 하는 메모리DB등 다양한 저장소에 저장 할 수가 있는데
+app.use(expressSession({
+    secret: 'my key',           //이때의 옵션은 세션에 세이브 정보를 저장할때 할때 파일을 만들꺼냐 , 아니면 미리 만들어 놓을꺼냐 등에 대한 옵션들임
+    resave: true,
+    saveUninitialized: true
+}));
+ 
+ 
+ 
+ 
+//라우트를 미들웨어에 등록하기 전에 라우터에 설정할 경로와 함수를 등록한다
+//
+//라우터를 사용 (특정 경로로 들어오는 요청에 대하여 함수를 수행 시킬 수가 있는 기능을 express 가 제공해 주는것)
+var router = express.Router();
+ 
+ 
+router.route('/process/login').post(
+    function (req, res) {
+        console.log('process/login 호출됨');
+        var paramID = req.body.id || req.query.id;
+        var paramPW = req.body.passwords || req.query.passwords;
+        console.log('paramID : ' + paramID + ', paramPW : ' + paramPW);
+ 
+        if (database) {
+            authUser(database, paramID, paramPW,
+                function (err, docs) {
+                    if (database)
+                    {
+                        if (err) {
+                            console.log('Error!!!');
+                            res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+                            res.write('<h1>에러발생</h1>');
+                            res.end();
+                            return;
+                        }
+ 
+                        if (docs) {
+                            console.dir(docs);
+                            res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+                            res.write('<h1>Login Success</h1>');
+                            res.write('<h1> user </h1>' + docs[0].id + '  :   ' + docs[0].name);
+                            res.write('<br><a href="/login.html"> re login </a>');
+                            res.end();
+ 
+                        }
+                        else {
+                            console.log('empty Error!!!');
+                            res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+                            res.write('<h1>user data not exist</h1>');
+                            res.write('<a href="/login.html"> re login</a>');
+                            res.end();
+                        }
+ 
+                    }
+                    else
+                    {
+                        console.log('DB 연결 안됨');
+                        res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+                        res.write('<h1>databasae 연결 안됨</h1>');
+                        res.end();
+                    }
+ 
+ 
+ 
+                }
+            );
+        }
+    }
+);
+ 
+ 
+router.route('/process/addUser').post(
+ 
+    function (req, res)
+    {
+        console.log('process/addUser 호출됨');
+        var paramID = req.body.id || req.query.id;
+        var paramPW = req.body.passwords || req.query.passwords;
+        var paramName = req.body.name || req.query.name;
+        console.log('paramID : ' + paramID + ', paramPW : ' + paramPW);
+ 
+        if (database)
+        {
+            addUser(database, paramID, paramPW, paramName,
+                function (err, result)
+                {
+                    if (err)
+                    {
+                        console.log('Error!!!');
+                        res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+                        res.write('<h1>에러발생</h1>');
+                        res.end();
+                        return;
+                    }
+ 
+                    if (result) {
+                        console.dir(result);
+                        res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+                        res.write('<h1>Add Success</h1>');
+                        res.write('<h1> name </h1>' + paramName);
+                        res.write('<br><a href="/login.html"> re login </a>');
+                        res.end();
+                    }
+                    else {
+                        console.log('추가 안됨 Error!!!');
+                        res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+                        res.write('<h1>can not add user</h1>');
+                        res.write('<a href="/login.html"> re login</a>');
+                        res.end();
+                    }
+ 
+                }
+            );
+        }
+        else
+        {
+            console.log('DB 연결 안됨');
+            res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+            res.write('<h1>databasae 연결 안됨</h1>');
+            res.end();
+        }
+       
+    }
+);
+ 
+ 
+//라우터 미들웨어 등록하는 구간에서는 라우터를 모두  등록한 이후에 다른 것을 세팅한다
+//그렇지 않으면 순서상 라우터 이외에 다른것이 먼저 실행될 수 있다
+app.use('/', router);       //라우트 미들웨어를 등록한다
+ 
+ 
+var authUser = function (db, id, password, callback) {
+    console.log('input id :' + id.toString() + '  :  pw : ' + password);
+ 
+    //cmd 에서 db.users  로 썻던 부분이 있는데 이때 이 컬럼(테이블)에 접근은 다음처럼 한다
+    var users = database.collection("users");
+ 
+    //찾고자 하는 정보를 입력해준다
+    //var result = users.find({ name: id, passwords: password });
+    //var result = users.find({ "name": id, "passwords":password });
+    //var result = users.find({ "name": id , "passwords": password });
+    //var result = users.find({});
+ 
+    var result = users.find({ "id": id, "passwords": password });
+ 
+    result.toArray(
+        function (err, docs) {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+ 
+            if (docs.length > 0) {
+                console.log('find user [ ' + docs + ' ]');
+                callback(null, docs);
+            }
+            else {
+                console.log('can not find user [ ' + docs + ' ]');
+                callback(null, null);
+            }
+        }
+ 
+    );
+ 
+};
+ 
+ 
+ 
+var addUser = function (db, id, passwords, name, callback) {
+    console.log('add User 호출됨' + id + '  , ' + passwords);
+    var users = db.collection('users');
+ 
+    //컬렉션에 데이터 추가할때는 배열 형태로 집어 넣는다
+    users.insertMany([{ "id": id, "passwords": passwords, "name": name }],
+        function (err, result) {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+ 
+            //데이터가 추가됐다면 insertedCount 카운트가 0 보다 큰값이 된다
+            if (result.insertedCount > 0) {
+                console.log('사용자 추가 됨' + result.insertedCount);
+                callback(null, result);
+            }
+            else {
+                console.log('사용자 추가 안됨' + result.insertedCount);
+                callback(null, null);
+ 
+            }
+ 
+        }
+    );
+ 
+};
+ 
+ 
+ 
+ 
+var errorHandler = expressErrorHandler(
+    { static: { '404': './public/404.html' } }              //404 에러 코드가 발생하면 해당 페이지를 보여주는 예외 미들웨어
+);
+ 
+app.use(expressErrorHandler.httpError(404));
+app.use(expressErrorHandler);
+ 
+//웹서버를 app 기반으로 생성
+var appServer = http.createServer(app);
+appServer.listen(app.get('port'),
+    function () {
+        console.log('express 웹서버 실행' + app.get('port'));
+        connectDB();        //DB 연결 , DB 연결 먼저해도 상관 없음
+    }
+);
+ 
